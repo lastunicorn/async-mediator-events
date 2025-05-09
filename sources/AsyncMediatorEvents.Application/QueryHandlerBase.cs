34bfcb6a57d765e340279fc51e672a2c0cf2089c
Alexandru Iuga <alex.iuga@gmail.com>
2025-05-09 11:53:00 +0300

    added a query handler base class

---
+++ sources/AsyncMediatorEvents.Application/QueryHandlerBase.cs
@@ -0,0 +1,20 @@
+﻿using AsyncMediator;
+
+namespace AsyncMediatorEvents.Application;
+
+internal abstract class QueryHandlerBase<TQuery, TResponse> : IQuery<TQuery, TResponse>
+{
+    protected IMediator Mediator { get; }
+
+    public QueryHandlerBase(IMediator mediator)
+    {
+        Mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+
+    public Task<TResponse> Query(TQuery criteria)
+    {
+        return DoHandle(criteria);
+    }
+
+    protected abstract Task<TResponse> DoHandle(TQuery criteria);
+}
